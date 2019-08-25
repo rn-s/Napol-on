39b09bd041ecc92660b892ab8cb8d993b6049f0e
kagamikarasu <karasu403@gmail.com>
2019-08-25 22:49:32 +0900

    HTTP/2.0 support

--- Elysion/src/main.go
+++ Elysion/src/main.go
@@ -12,5 +12,6 @@ func main() {
 		return c.JSON(http.StatusOK, map[string]string{"hello": "world"})
 	})
 
-	e.Logger.Fatal(e.Start(":80"))
+	//e.Logger.Fatal(e.Start(":80"))
+	e.Logger.Fatal(e.StartTLS(":443", "cert.pem", "key.pem"))
 }

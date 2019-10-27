af394795def932bed8a016f76e94d0b278ae988d
武田雄也 <karasu403@gmail.com>
2019-10-27 22:34:48 +0900

    詳細情報

--- Lyon/shop/models.py
+++ Lyon/shop/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
 from django.db import models
+from ppretty import ppretty
 
 
 class Basic(models.Model):
+    id = models.BigIntegerField(primary_key=True)
     data_source = models.IntegerField(verbose_name='取得元')
     name = models.CharField(verbose_name='店舗名', max_length=255)
     lat = models.FloatField(verbose_name='緯度')
@@ -27,27 +29,61 @@ class Google(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     basic_id = models.ForeignKey(Basic, db_column='basic_id', on_delete=models.CASCADE)
     place_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, null=True)
+    tel = models.CharField(max_length=255, null=True)
+    types = models.CharField(max_length=255, null=True)
+    price_level = models.IntegerField(null=True)
+    rating = models.FloatField(null=True)
+    user_ratings_total = models.IntegerField(null=True)
+    url = models.CharField(max_length=255, null=True)
 
     def setBasicValues(self, x):
         # Google ID
         self.id = x.id
         # Google Place ID
         self.place_id = x.place_id
+        # 店舗名
+        self.name = x.name
+        # 作成日
+        self.created = datetime.now()
+        # 基本情報
+        basic = Basic()
+        basic.setValues(x, 1)
+        # 外部キー格納
+        self.basic = basic
+
+    def setDetailValues(self, x):
+        # Google ID
+        self.id = x.id
+        # Google Place ID
+        self.place_id = x.place_id
+        # 名前
+        self.name = x.name
+        # 住所
+        self.address = x.formatted_address
+        # 電話番号
+        self.tel = x.formatted_phone_number
         # Tags
         self.types = []
         for i in range(len(x.types)):
             self.types.append(x.types[i])
         self.types = ','.join(self.types)
-
+        # 価格帯
+        self.price_level = x.price_level
+        # 評価
+        self.rating = x.rating
+        # 評価数
+        self.user_ratings_total = x.user_ratings_total
+        # url
+        self.url = x.url
+        # 作成日
         self.created = datetime.now()
-
-        basic = Basic()
-        basic.setValues(x, 0)
-        self.basic = basic
 
     def save(self, **kwargs):
         # 重複チェック
         result = Google.objects.filter(id=self.id).first()
+        # 存在しない新規追加
         if result is None:
             # 基本IDの最大値+1をキーとする
             self.basic.id = Basic.objects.count() + 1
@@ -57,6 +93,13 @@ class Google(models.Model):
             self.basic.save()
             # 自身の保存
             super(Google, self).save(**kwargs)
+            # 結果返却
+            return Google.objects.filter(id=self.id).first()
+        else:
+            # 更新
+            self.basic_id = result.basic_id
+            super(Google, self).save(**kwargs)
+        return result
 
 
 class GoogleNearByResponse:
--- Lyon/shop/views.py
+++ Lyon/shop/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .serializer import ShopSerializer
-from .models import Basic, GoogleNearByResponse, GoogleDetailByResponse
+from .models import Basic, Google, GoogleNearByResponse, GoogleDetailByResponse
 from ppretty import ppretty
 
 
@@ -46,17 +46,26 @@ class ShopViewSet(viewsets.ModelViewSet):
             )
         )
 
+        # 各店舗単位で処理
         for i in range(len(google_response.results)):
-            # print(ppretty(Google.objects.get(id=1)))
-            print(ppretty(google_response.results[i]))
-            google_response.results[i].save()
+            # 基本情報の保存
+            result = google_response.results[i].save()
 
-        return Response("")
+            # 詳細情報の取得及び保存
+            self.update_google_detail(result.id)
 
-    @action(methods=['get'], detail=False)
-    def update_google_detail(self, request):
+        return Response("Success")
+
+    def update_google_detail(self, id):
         # response = requests.get("https://maps.googleapis.com/maps/api/place/details/json?place_id
         # =ChIJze3VB9CGGGARKQzMMylJ3Io&language=ja&key=AIzaSyCJmfQzxAUw9YOOiJfG-vrZ1Oo4cGvdqQA")
+
+        query_set = Google.objects.filter(id=id)
+
+        if query_set.first() is None:
+            return None
+        shop_google_current = query_set.first()
+
         json_raw = open("google_detail_sample.txt")
 
         google_response = GoogleDetailByResponse(
@@ -70,4 +79,10 @@ class ShopViewSet(viewsets.ModelViewSet):
                 (*d.values())
             )
         )
-        return Response("")
+
+        # 結果とIDが一致しなければ無視
+        if google_response.result.id != shop_google_current.id:
+            print("None")
+            return None
+
+        google_response.result.save()
